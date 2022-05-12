e791bc651010809554ad98890d03c3efe243a332
aorri2 <76669404+aorri2@users.noreply.github.com>
2022-05-12 13:36:07 +0900

    Service Layer add

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,24 @@ import java.util.List;
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+
+    @Autowired
+    TodoService service;
+
     @GetMapping
-    public ResponseEntity<?> responseEntity(){
+    public ResponseEntity<?> responseEntity() {
         List<String> list = new ArrayList<>();
         list.add("반갑습니다~~~ 200번 Status 입니다욤");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo(){
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
     }
