b79b8c10258c704e5e223be0fcb320e03adbb8c0
aorri2 <aorri96@naver.com>
2022-05-13 21:31:59 +0900

    Post 추가

---
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -0,0 +1,26 @@
+package com.example.demo.persistence;
+
+import com.example.demo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity,String> {
+    List<TodoEntity> findByUserId(String userId);
+    /**
+     이 메서드를 작성하면 스프링 데이터 JPA가 메서드 이름을 파싱해서 SELECT * FROM TodoRepository WHERE userId = '{userId}'
+     와 같은 쿼리를 작성해 실행한다.
+
+     더 복잡한 쿼리는 @Query 어노테이션을 사용해 지정할 수 있다.
+
+     메서드 이름 작성 방법과 예제(레퍼런스)
+     https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+     */
+    //?1은 메서드의 매개변수의 순서 위치다.
+//    @Query("select * from Todo t where t.userId =?1")
+//    List<TodoEntity> findByUserId(String userId);
+
+}
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,55 @@
 package com.example.demo.service;
 
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
 
-    public String testService(){
-        return "Test Service";
+    @Autowired
+    private TodoRepository repository;
+
+
+    public String testService()
+    {
+        //TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+
+        //TodoEntity 저장
+        repository.save(entity);
+
+        //TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        return savedEntity.getTitle();
+
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        //Validations 검증 단계
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity id : {} is saved",entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+
+    }
+    private void validate(final TodoEntity entity){
+        if(entity == null){
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+        if(entity.getUserId() == null){
+            log.warn("Unknown user");
+            throw new RuntimeException("Unknown user");
+        }
     }
 }
